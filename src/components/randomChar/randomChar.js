83cb7f405bd26c8055d86b732d1da08cdc726078
Aliaksei <dekomessi@mail.ru>
2021-06-30 17:07:24 +0300

    third commit

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -11,29 +11,21 @@ export default class RandomChar extends Component {
 
     gotService = new gotService();
     state = {
-        name: null,
-        gender: null,
-        born: null,
-        died: null,
-        culture: null
+        char: {}
+    }
+
+    onCharacterLoaded = (char) => {
+        this.setState({char});
     }
 
     updateCharacter() {
-        const id = 100;
+        const id = Math.floor(Math.random() * 140 + 25); //25-140
         this.gotService.getCharacter(id)
-            .then((char) => {
-                this.setState({
-                    name: char.name,
-                    gender: char.gender,
-                    born: char.born,
-                    died: char.died,
-                    culture: char.culture
-                })
-            });
+            .then(this.onCharacterLoaded);
     }
 
     render() {
-        const {name, gender, born, died, culture} = this.state;
+        const { char: { name, gender, born, died, culture }} = this.state;
 
         return (
             <div className="random-block rounded">
