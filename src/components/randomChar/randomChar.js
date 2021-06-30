97b6d9353696e63201fa253acce5ce82fd909c5e
Aliaksei <dekomessi@mail.ru>
2021-07-01 00:25:41 +0300

    fourst commit was added spinner

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
+import Spinner from '../spinner';
 
 export default class RandomChar extends Component {
 
@@ -11,44 +12,74 @@ export default class RandomChar extends Component {
 
     gotService = new gotService();
     state = {
-        char: {}
+        char: {},
+        loading: true
     }
 
     onCharacterLoaded = (char) => {
-        this.setState({char});
+        this.setState({
+            char,
+            loading: false
+        });
+    }
+
+    onError = (err) => {
+        this.setState({
+            char,
+            loading: false
+        });
     }
 
     updateCharacter() {
         const id = Math.floor(Math.random() * 140 + 25); //25-140
         this.gotService.getCharacter(id)
-            .then(this.onCharacterLoaded);
+            .then(this.onCharacterLoaded)
+            .catch(this.onError);
     }
 
     render() {
-        const { char: { name, gender, born, died, culture }} = this.state;
+        const { char, loading } = this.state;
+        const  content = loading ? <Spinner/> : <View char={char}/>;
+
+        if (loading) {
+            return <Spinner/>
+        }
 
         return (
             <div className="random-block rounded">
-                <h4>Random Character: {name}</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>{gender}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>{born}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>{died}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>{culture}</span>
-                    </li>
-                </ul>
+                {content}
             </div>
         );
     }
+}
+
+// const View = () => {
+//     const {name, gender, born, died, culture} = this.props.char;
+
+const View = ({char}) => {
+
+    const {name, gender, born, died, culture} = char;
+    return (
+        <>
+            <h4>Random Character: {name}</h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Gender </span>
+                    <span>{gender}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Born </span>
+                    <span>{born}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Died </span>
+                    <span>{died}</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between">
+                    <span className="term">Culture </span>
+                    <span>{culture}</span>
+                </li>
+            </ul>
+        </>
+    )
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+    <div class="loadingio-spinner-spin-v4mdyc5tt3"><div class="ldio-u57nc7x3rrb">
+        <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+    </div></div>
+    )
+}
+
+export default Spinner;
+
